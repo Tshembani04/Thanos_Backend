54e605a7ab39608c556f9cebe13934b7c03b5404
Tshembani04 <aurthurmhlongo.ta@gmail.com>
2024-04-18 11:28:00 +0200

    deploy both backend and frontend

--- backend/index.js
+++ backend/index.js
@@ -9,49 +9,61 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/getTokens", async (req, res) => {
-  const { address, chain } = req.query;
+  try {
+    const { address, chain } = req.query;
 
-  const tokens = await Moralis.EvmApi.token.getWalletTokenBalances({
-    chain: chain,
-    address: address,
-  });
+    // Log the received parameters
+    console.log("Received request with address:", address, "and chain:", chain);
 
-  const nfts = await Moralis.EvmApi.nft.getWalletNFTs({
-    chain: chain,
-    address: address,
-    mediaItems: true,
-  });
-
-  console.log(nfts.raw.result);
-
-  const myNfts = nfts.raw.result.map((e, i) => {
-    if (
-      e?.media?.media_collection?.high?.url &&
-      !e.possible_spam &&
-      e?.media?.category !== "video"
-    ) {
-      return e["media"]["media_collection"]["high"]["url"];
+    // Check if the chain parameter is a valid string
+    if (typeof chain !== "string") {
+      return res.status(400).json({ error: "Invalid chain parameter" });
     }
-  });
 
-  const balance = await Moralis.EvmApi.balance.getNativeBalance({
-    chain: chain,
-    address: address,
-  });
+    const tokens = await Moralis.EvmApi.token.getWalletTokenBalances({
+      chain: chain,
+      address: address,
+    });
 
-  const jsonResponse = {
-    tokens: tokens.raw,
-    nfts: myNfts,
-    balance: balance.raw.balance / 10 ** 18,
-  };
+    const nfts = await Moralis.EvmApi.nft.getWalletNFTs({
+      chain: chain,
+      address: address,
+      mediaItems: true,
+    });
 
-  return res.status(200).json(jsonResponse);
+    console.log(nfts.raw.result);
+
+    const myNfts = nfts.raw.result
+      .filter(
+        (e) =>
+          e?.media?.media_collection?.high?.url &&
+          !e.possible_spam &&
+          e?.media?.category !== "video"
+      )
+      .map((e) => e["media"]["media_collection"]["high"]["url"]);
+
+    const balance = await Moralis.EvmApi.balance.getNativeBalance({
+      chain: chain,
+      address: address,
+    });
+
+    const jsonResponse = {
+      tokens: tokens.raw,
+      nfts: myNfts,
+      balance: balance.raw.balance / 10 ** 18,
+    };
+
+    return res.status(200).json(jsonResponse);
+  } catch (error) {
+    console.error("Error:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 Moralis.start({
   apiKey: process.env.MORALIS_KEY,
 }).then(() => {
   app.listen(port, () => {
-    console.log(`Listening for API Calls`);
+    console.log(`Listening for API Calls on port ${port}`);
   });
 });
